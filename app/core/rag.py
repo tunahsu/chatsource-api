adee49d4caa1fd956091375652039aaad7e7ca80
Tuna Hsu <tunahsu45@gmail.com>
2024-10-11 13:36:15 +0800

    Integrate  as the new query engine

--- app/core/rag.py
+++ app/core/rag.py
@@ -10,8 +10,9 @@ from app.core.config import settings
 import openai
 from llama_index.llms.openai import OpenAI
 from llama_index.embeddings.openai import OpenAIEmbedding
-from llama_index.core import VectorStoreIndex
-from llama_index.core import Settings, Document as LIDocument
+from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.core import VectorStoreIndex, Settings, Document as LIDocument
+from llama_index.core.retrievers import VectorIndexRetriever
 
 openai.api_key = settings.OPENAI_API_KEY
 
@@ -36,7 +37,11 @@ async def get_query_engine(query: ChatbotQueryRequest,
                  temperature=chatbot.temperature,
                  system_prompt=chatbot.instruction)
     index = VectorStoreIndex(documents, show_progress=True)
-    query_engine = index.as_query_engine(llm=llm)
+    retriever = VectorIndexRetriever(
+        index=index,
+        similarity_top_k=3,
+    )
+    query_engine = RetrieverQueryEngine.from_args(retriever=retriever, llm=llm)
     try:
         yield query_engine
     finally:
